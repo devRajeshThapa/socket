3a6a8668374826183d4d78625955e0ce45b66e9c
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-01-19 00:58:12 +0545

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -47,10 +47,10 @@ int main() {
         }
 
         char buffer[1024] = {0};
-        int bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (bytes_received > 0) {
+        int bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+        if (bytes_read > 0) {
             std::cout << "Message from client: " << buffer << std::endl;
-        } else if (bytes_received < 0) {
+        } else if (bytes_read < 0) {
             std::cerr << "Receive failed: " << strerror(errno) << std::endl;
         }
 
